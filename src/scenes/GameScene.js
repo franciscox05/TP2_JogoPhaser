ca8fb3b922da5cad2cf7e7efbe6ee31429ec0de2
franciscox05 <franciscomlg4@gmail.com>
2026-05-15 14:21:12 +0100

    Melhora gameplay core com paredes, colisoes e patrulha

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -24,6 +24,11 @@
     graphics.fillStyle(0x4caf50, 1);
     graphics.fillRect(0, 0, 30, 30);
     graphics.generateTexture("doorTex", 30, 30);
+    graphics.clear();
+
+    graphics.fillStyle(0x3b2d25, 1);
+    graphics.fillRect(0, 0, 40, 40);
+    graphics.generateTexture("wallTex", 40, 40);
     graphics.destroy();
 
     this.physics.world.setBounds(50, 50, 860, 440);
@@ -34,11 +39,18 @@
     this.keyItem = this.physics.add.staticSprite(820, 100, "keyTex");
     this.exitDoor = this.physics.add.staticSprite(840, 440, "doorTex");
 
+    this.walls = this.physics.add.staticGroup();
+    this.createWalls();
+
     this.enemies = this.physics.add.group();
     const enemyA = this.enemies.create(300, 270, "enemyTex");
     const enemyB = this.enemies.create(620, 200, "enemyTex");
     enemyA.setVelocityX(110).setBounce(1, 1).setCollideWorldBounds(true);
     enemyB.setVelocityY(130).setBounce(1, 1).setCollideWorldBounds(true);
+
+    this.physics.add.collider(this.player, this.walls);
+    this.physics.add.collider(this.enemies, this.walls);
+    this.physics.add.collider(this.enemies, this.enemies);
 
     this.hasKey = false;
     this.lives = 3;
@@ -75,10 +87,28 @@
       color: "#cbb89d"
     }).setScrollFactor(0);
 
+    this.doorHintText = this.add.text(480, 510, "", {
+      fontSize: "18px",
+      color: "#f2e6d8"
+    }).setOrigin(0.5);
+
     this.toggleLangKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.E);
     this.refreshHud();
 
     this.soundPlayed = false;
+  }
+
+  createWalls() {
+    const wallPositions = [];
+
+    for (let x = 180; x <= 780; x += 40) wallPositions.push([x, 160]);
+    for (let x = 180; x <= 740; x += 40) wallPositions.push([x, 320]);
+    for (let y = 200; y <= 400; y += 40) wallPositions.push([420, y]);
+    for (let y = 80; y <= 280; y += 40) wallPositions.push([700, y]);
+
+    wallPositions.forEach(([x, y]) => {
+      this.walls.create(x, y, "wallTex");
+    });
   }
 
   t(key) {
@@ -92,6 +122,9 @@
     this.hudText.setText(
       `${this.t("hudLives")}: ${this.lives}   ${this.t("hudTime")}: ${this.timeLeft}   ${this.t("hudKey")}: ${keyState}`
     );
+    this.doorHintText.setText(
+      this.hasKey ? "Saida desbloqueada / Exit unlocked" : "Precisas da chave / You need the key"
+    );
   }
 
   handleCollectKey() {
@@ -99,7 +132,6 @@
     this.hasKey = true;
     this.keyItem.destroy();
     this.refreshHud();
-    // Placeholder integration for sound requirement.
     if (!this.soundPlayed) {
       this.soundPlayed = true;
     }
@@ -116,6 +148,7 @@
     this.hitCooldown = true;
 
     this.lives -= 1;
+    this.player.setPosition(100, 100);
     this.refreshHud();
 
     this.player.setTint(0xff0000);
