c5f5cc2516220bcd6516c2656de1847059eafc1d
AfonsoBarbosa01 <afonsofreitasbarbosa@gmail.com>
2026-05-22 00:40:08 +0100

    Refatoração da estrada: táxis dinâmicos e limpeza de elementos da masmorra

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -6,6 +6,16 @@ export class BootScene extends Phaser.Scene {
     super("BootScene");
   }
 
+  preload() {
+    this.load.audio("engineStart", "./assets/audio/start.mp3");
+    this.load.svg("carroSprite", "./assets/images/carro.svg");
+    this.load.svg("taxiSprite", "./assets/images/taxi.svg");
+    
+    // Adiciona estas duas linhas:
+    this.load.svg("gasolinaSprite", "./assets/images/gasolina.svg");
+    this.load.svg("metaSprite", "./assets/images/meta.svg");
+  }
+
   create() {
     this.registry.set("lang", "pt");
     this.registry.set("i18n", { pt, en });
--- src/scenes/MenuScene.js
+++ src/scenes/MenuScene.js
@@ -92,7 +92,7 @@ export class MenuScene extends Phaser.Scene {
     }
 
     if (Phaser.Input.Keyboard.JustDown(this.enterKey)) {
-      this.registry.set("runState", { lives: 3, timeLeft: 180, keys: 0 });
+      this.sound.play("engineStart"); // Toca o som do motor
       this.scene.start("Room1Scene");
     }
   }
